1a6545de1b38c56776442f972d32cf38dcf0093e
rebeckaspolander <rebeckaspolander@yahoo.se>
2023-11-25 19:54:19 +0000

    fix

--- infra/main.tf
+++ infra/main.tf
@@ -76,6 +76,6 @@ resource "aws_sns_topic" "user_updates" {
 }
 
 module "alarm" {
-  source = "./alarm"
-  sns_topic_arn = aws_sns_topic.user_updates.arn
+  source        = "./alarm"
+  //sns_topic_arn = aws_sns_topic.user_updates.arn
 }
